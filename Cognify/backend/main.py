a30c680561477e635f0ca24e17d194898eea5d4b
URISHITA ARORA <urishitaarora@gmail.com>
2026-08-11 18:37:33 +0530

    Add CORS origin for production frontend

--- Cognify/backend/main.py
+++ Cognify/backend/main.py
@@ -56,6 +56,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://cognify-frontend-bggv.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
